584b5fc5eca59c6c87608c3de90f7692657a77dd
lsf <lsf_2012@163.com>
2019-02-11 23:41:59 +0800

    dailyfresh_day07

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from order.views import OrderPlaceView, OrderCommitView
 
 app_name = "order"
 
 urlpatterns = [
-   
+    path('place', OrderPlaceView.as_view(), name="orderplace"),
+    path('commit', OrderCommitView.as_view(), name="ordercommit")
 ]
